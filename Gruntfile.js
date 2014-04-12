53c5120f5785a473ef53967f02709b8de6778e3c
Kyle Conkright <kyleconkright@gmail.com>
2014-04-12 00:02:00 -0700

    A shopify Theme using Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  	grunt.initConfig({
+	    pkg: grunt.file.readJSON('package.json'),
+
+	    sass: {
+	    	dist: {
+		      	options: {
+		        	style: 'compressed'
+		      	},
+		      	files: {
+		        	'tmp/style.css': 'lib/style.scss'
+		      	}
+		    }
+		},
+		cssmin: {
+		  	combine: {
+		    	files: {
+		      		'tmp/stylemin.css': ['lib/vendor/lemonade.css', 'tmp/style.css']
+		    	}
+		  	}
+		},
+		autoprefixer: {
+            dist: {
+                files: {
+                    'soundfreaq-theme-7783581/assets/style.css.liquid':['tmp/stylemin.css']
+                }
+            }
+        },
+        coffee: {
+          compile: {
+	            files: {
+	              	'tmp/script.js': 'lib/script.coffee'
+	            }
+          	}
+     	},
+     	uglify: {
+      		my_target: {
+      		    files: {
+      		    	'soundfreaq-theme-7783581/assets/script.js.liquid': ['lib/vendor/jquery.js','lib/vendor/lightbox.js','lib/vendor/slides.js','tmp/script.js'],
+      		    }
+      		}
+	    },
+	    watch: {
+	     	css: {
+	        	files: 'lib/*.scss',
+	        	tasks: ['sass','cssmin','autoprefixer'],
+	        	options: {
+	          		livereload: true,
+	        	},
+	      	},
+	      	scripts: {
+	      	    files: 'lib/*.coffee',
+	      	    tasks: ['coffee','uglify']
+	      	},
+	    }
+
+  	});
+
+  // Load the plugin that provides the "uglify" task.
+  
+  	grunt.loadNpmTasks('grunt-contrib-uglify');
+  	grunt.loadNpmTasks('grunt-contrib-sass');
+  	grunt.loadNpmTasks('grunt-autoprefixer');
+  	grunt.loadNpmTasks('grunt-contrib-coffee');
+  	grunt.loadNpmTasks('grunt-contrib-watch');
+  	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  	// Default task(s).
+  	grunt.registerTask('default', ['uglify','sass','autoprefixer','coffee','watch','cssmin']);
+
+
+};
