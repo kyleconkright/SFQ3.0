f96c0109d923f3d2e6557b6e3c33e8bc0829c973
Kyle Conkright <kyleconkright@gmail.com>
2014-04-13 19:22:58 -0700

    Mailchimp Integration Set
    with AJAX (need to switch to SFQ account)

--- lib/script.coffee
+++ lib/script.coffee
@@ -2,3 +2,22 @@ $ ->
 	console.log 'scripts are working'
 
 	$('div#header').scrollToFixed()
+
+	$form = $('#newsletter form')
+
+	$form.submit (e)->
+		e.preventDefault()
+		$this = $(@)
+		$.ajax
+			type: "GET"
+			url: 'http://erichutchinson.us7.list-manage.com/subscribe/post-json?u=c8c95e39b427209a753338e7a&amp;id=3e4a2c0d22&c=?'
+			data: $this.serialize()
+			dataType: 'json'
+			contentType: "application/json; charset=utf-8"
+			error: (err) ->
+				alert 'no'
+			success: (data) ->
+				if ($('input[name=EMAIL]').val().indexOf('@') != -1 and $('input[name=EMAIL]').val().indexOf('.') != -1)
+					$('label').text('Thanks! Check your inbox to confirm.')
+				else 
+					alert 'not valid email'
--- tmp/script.js
+++ tmp/script.js
@@ -1,7 +1,31 @@
 (function() {
   $(function() {
+    var $form;
     console.log('scripts are working');
-    return $('div#header').scrollToFixed();
+    $('div#header').scrollToFixed();
+    $form = $('#newsletter form');
+    return $form.submit(function(e) {
+      var $this;
+      e.preventDefault();
+      $this = $(this);
+      return $.ajax({
+        type: "GET",
+        url: 'http://erichutchinson.us7.list-manage.com/subscribe/post-json?u=c8c95e39b427209a753338e7a&amp;id=3e4a2c0d22&c=?',
+        data: $this.serialize(),
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        error: function(err) {
+          return alert('no');
+        },
+        success: function(data) {
+          if ($('input[name=EMAIL]').val().indexOf('@') !== -1 && $('input[name=EMAIL]').val().indexOf('.') !== -1) {
+            return $('label').text('Thanks! Check your inbox to confirm.');
+          } else {
+            return alert('not valid email');
+          }
+        }
+      });
+    });
   });
 
 }).call(this);
